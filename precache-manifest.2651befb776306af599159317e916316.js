aa125f3fc011077501301fc404a6a840b07900b3
Gil Atari <gil.atari@gmail.com>
2020-01-07 15:46:15 +0200

    Updates

---
+++ precache-manifest.2651befb776306af599159317e916316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1445ef4cdf26ed0f87ecd39d7a6ea033",
+    "url": "/herolo/index.html"
+  },
+  {
+    "revision": "623b297e4eae2f4218f8",
+    "url": "/herolo/static/css/main.4bf481c4.chunk.css"
+  },
+  {
+    "revision": "35050770fb1b3374256a",
+    "url": "/herolo/static/js/2.978c95d4.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/herolo/static/js/2.978c95d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "623b297e4eae2f4218f8",
+    "url": "/herolo/static/js/main.9cf7504b.chunk.js"
+  },
+  {
+    "revision": "690cb6805dd39497b3ae",
+    "url": "/herolo/static/js/runtime-main.4f75c491.js"
+  }
+]);
