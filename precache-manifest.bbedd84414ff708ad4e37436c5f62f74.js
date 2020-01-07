77e564a2576ccf6db3c0a2a915622cb1740af62b
Gil Atari <gil.atari@gmail.com>
2020-01-07 15:50:40 +0200

    Updates

---
+++ precache-manifest.bbedd84414ff708ad4e37436c5f62f74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a060de0297c59cbbd770cf5ca1040c4",
+    "url": "/Gil-Atari-07-01-2018/index.html"
+  },
+  {
+    "revision": "250ef3bab4b41cb907e3",
+    "url": "/Gil-Atari-07-01-2018/static/css/main.4bf481c4.chunk.css"
+  },
+  {
+    "revision": "35050770fb1b3374256a",
+    "url": "/Gil-Atari-07-01-2018/static/js/2.978c95d4.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/Gil-Atari-07-01-2018/static/js/2.978c95d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "250ef3bab4b41cb907e3",
+    "url": "/Gil-Atari-07-01-2018/static/js/main.18b1a747.chunk.js"
+  },
+  {
+    "revision": "cb44061c9611be0cb863",
+    "url": "/Gil-Atari-07-01-2018/static/js/runtime-main.6b7c12bc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/herolo/precache-manifest.2651befb776306af599159317e916316.js"
+  "/Gil-Atari-07-01-2018/precache-manifest.bbedd84414ff708ad4e37436c5f62f74.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/herolo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Gil-Atari-07-01-2018/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
